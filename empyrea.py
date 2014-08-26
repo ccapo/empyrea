8c6c2cf67001e0fbc80aa2c3973938782c3cac60
Joachim de Groot <jdegroot@web.de>
2014-08-26 21:57:49 +0200

    Factor out thesaurus.

--- empyrea.py
+++ empyrea.py
@@ -15,6 +15,7 @@ import empyrea.audio
 from empyrea.namegen import MakeLang, MakeName
 import empyrea.inflect as inflect
 from empyrea.utils import *
+from empyrea.thesaurus import t
 
 os.putenv("SDL_VIDEO_CENTERED", "1")
 sys.setrecursionlimit(50000)
@@ -67,8 +68,6 @@ __version__ = '0.3 Alpha Tech Demo by ' + __author__
 ##       - c. WorldAtlas
 ##   XIV. Main Game Architecture
 ##       - c. PlayWorld, c. Pathfinder
-##    XV. Thesaurus Entries
-##       - f. t
 ##   XVI. Main Worldgen Loop
 ##       - f. doWorldGen
 ##  XVII. Main World Atlas Loop
@@ -18503,154 +18502,6 @@ class Pathfinder(threading.Thread):
         if not self.killswitch:
             hpcswitch = True
 
-####################################
-# XV. Thesaurus Entries            #
-####################################
-
-thesaurus = {}
-
-# Colors ========================= #
-
-thesaurus['black'] = ['black','charcoal black','coal black','ebon','ebony','jet','onyx','sable']
-thesaurus['blue'] = ['azure','beryl','blue-gray','blue-green','cerulean','cobalt','indigo','navy','royal blue','sapphire','teal','turquoise', \
-                     'aquamarine','periwinkle']
-thesaurus['brown'] = ['almond','brown','amber','auburn','beige','bronze','burnt sienna','chestnut','chocolate','cinnamon','cocoa brown','copper brown', \
-                      'ginger brown','hazel','mahogany brown','ochre','puce','russet','sepia','tan','tawny','umber','sandy brown']
-thesaurus['green'] = ['green','emerald green','beryl','forest green','chartreuse','jade green','lime green','pea green','moss green','olive green','sage green', \
-                      'viridian']
-thesaurus['gray'] = ['gray','ashen gray','cloudy gray','dove gray','dusky gray','granite gray','iron gray','leaden gray','pepper-gray','silver-gray', \
-                     'slate gray','smoky gray']
-thesaurus['orange'] = ['orange','goldenrod','burnt orange','tangerine-orange']
-thesaurus['purple'] = ['purple','royal purple','amaranthine','amethyst','violet','blue-violet','pomegranate purple','mulberry purple','mauve','magenta','lavender','lilac purple', \
-                       'heliotrope','orchid purple','periwinkle','perse']
-thesaurus['red'] = ['red','blood red','cherry red','cardinal red','crimson','garnet red','magenta','maroon','coral red','puce','pink','rose red','rust red', \
-                    'salmon pink','scarlet','vermilion','wine red']
-thesaurus['silver'] = ['pearly silver','silver','sterling silver']
-thesaurus['tan'] = ['peach','tan','cream-colored','beige','taupe']
-thesaurus['white'] = ['white','chalky white','milky white','ivory white','snowy white','hoary white','off-white']
-thesaurus['yellow'] = ['yellow','blonde','citron','canary yellow','creamy yellow','golden yellow','honey yellow','primrose yellow','straw yellow','sulfur yellow','tawny yellow','puce', \
-                       'gold','caramel','flaxen yellow']
-
-# Color Modifiers ================ #
-
-thesaurus['pale'] = ['pale','blanched','faint','dim','faded','palid','waxen','sickly','cool']
-thesaurus['bright'] = ['bright','blazing','dazzling','effulgent','glossy','radiant','lustrous','vivid']
-thesaurus['dull'] = ['drab','dull','bleak','dreary','lusterless','faded','murky','somber']
-thesaurus['neon'] = ['neon','iridescent','phosphorescent','softly glowing']
-thesaurus['flecked'] = ['flecked','speckled','sprinkled']
-thesaurus['striped'] = ['striped','banded','straked']
-thesaurus['patched'] = ['patched','blotched','daubed','smeared','blotted','mottled','spattered','splotched','dappled']
-thesaurus['spotted'] = ['spotted','dotted','bespotted']
-thesaurus['tipped'] = ['tipped','topped']
-
-# Creature Reproduction =========== #
-
-thesaurus['crep-1'] = ['almost never reproduces','only reproduces once in a blue moon']
-thesaurus['crep-2'] = ['reproduces extremely rarely']
-thesaurus['crep-3'] = ['reproduces very rarely','very rarely reproduces']
-thesaurus['crep-4'] = ['rarely reproduces','reproduces infrequently','seldomly reproduces']
-thesaurus['crep-5'] = ['reproduces sporadically','reproduces semi-regularly']
-thesaurus['crep-6'] = ['reproduces frequently','reproduces somewhat frequently']
-thesaurus['crep-7'] = ['reproduces quite often','reproduces fairly often']
-thesaurus['crep-8'] = ['reproduces enthusiastically and often','reproduces very often']
-thesaurus['crep-9'] = ['reproduces at an astounding rate','reproduces extremely often']
-thesaurus['crep-10'] = ['never seems to stop reproducing','constantly reproduces']
-
-# ================================= #
-
-thesaurus['aggwords'] = ['aggressive','belligerent','ferocious','vicious','bellicose','quarrelsome']
-thesaurus['angeredwords'] = ['angered','antagonized','irritated','aggravated','upset']
-thesaurus['appextingwords'] = ['extending','protruding','stretching','originating','emerging']
-thesaurus['appextwords'] = ['extend','protrude','stretch','originate','emerge']
-thesaurus['augustwords'] = ['august','remarkable','imposing','great','preeminent','prominent']
-thesaurus['averagewords'] = ['average','typical','common','middling','unexceptional','ordinary']
-thesaurus['avoidedwords'] = ['avoided','evaded','left alone','left to its own devices','steered clear of']
-thesaurus['avoidwords'] = ['avoid','steer clear of','stay away from','keep clear of']
-
-thesaurus['becausewords'] = ['because of','due to','as a result of','as a consequence of']
-thesaurus['beholdwords'] = ['behold','lay eyes on','gaze upon','witness']
-thesaurus['breachedwords'] = ['breached','violated','infringed upon','intruded upon','trespassed upon']
-thesaurus['breachingwords'] = ['breaching','violating','infringing upon','intruding upon','trespassing upon']
-thesaurus['breachwords'] = ['breach','violate','infringe upon','intrude upon','trespass upon']
-
-thesaurus['careaboutwords'] = ['care about','concern itself with','bother with','trouble itself with','bother itself over','trouble itself over', \
-                  'worry about']
-thesaurus['clawwords'] = ['claw','talon','nail']
-thesaurus['concernedwords'] = ['concerned','preoccupied','consumed','involved','engaged','engrossed']
-thesaurus['cwords'] = ['creature','animal','beast']
-thesaurus['cwordsplur'] = ['creatures','animals','beasts']
-
-thesaurus['dangerouswords'] = ['dangerous','perilous','hazardous']
-thesaurus['dangerwords'] = ['danger','threat','hazard','peril','risk','menace']
-thesaurus['despisedwords'] = ['despised','abhorred','detested','hated','loathed']
-thesaurus['despitewords'] = ['despite','in spite of','regardless of']
-thesaurus['dietwords'] = ['diet','food preferences','feeding habits','feeding preferences','dietary preferences']
-thesaurus['digitwords'] = ['digit','finger']
-thesaurus['dividewords'] = ['divided into','comprised of','made up of','separated into']
-thesaurus['docilewords'] = ['docile','amicable','peaceful','placid','nonbelligerent','easygoing','tranquil','peaceable','friendly']
-thesaurus['dwellswords'] = ['dwells','lives','abides','resides']
-thesaurus['dwellingwords'] = ['dwelling','inhabiting','abiding','living','residing']
-
-thesaurus['encounteredwords'] = ['encountered','met','confronted','chanced upon','crossed']
-thesaurus['encounterwords'] = ['encounter','meet','confront','chance upon','come across','run across','cross']
-thesaurus['endingwords'] = ['ending','terminating','culminating']
-thesaurus['endwords'] = ['end','terminate','culminate']
-
-thesaurus['generallywords'] = ['generally','usually','typically','normally']
-
-thesaurus['habitswords'] = ['habits','tendencies','mannerisms','proclivities','propensities','ways']
-thesaurus['hasknownwords'] = ['has been said to have','is reputed to have','is said to have','is rumored to have','has been rumored to have']
-thesaurus['howeverwords'] = ['however','all the same','be that as it may','nonetheless']
-
-thesaurus['illtempwords'] = ['ill-tempered','cantankerous','irascible','touchy','acrimonious']
-thesaurus['immensewords'] = ['immense','colossal','elephantine','enormous','gigantic','mammoth','monstrous']
-thesaurus['inclinedwords'] = ['inclined to be','inclined towards being','disposed towards being','predisposed towards being','given to being']
-thesaurus['inhabitswords'] = ['inhabits','populates','dwells in','abides in','resides in','dwells in']
-thesaurus['iswords'] = ['is','can be','may be','tends to be','is known to be','is reputedly','has a reputation for being','is said to be', \
-           'is regarded as being','is judged to be','is reckoned to be']
-
-thesaurus['knowntowords'] = ['known to','reputed to','said to','rumored to','reputed to']
-
-thesaurus['mainwords'] = ['main','primary','principal','major']
-thesaurus['mainlywords'] = ['mainly','primarily','principally','mostly']
-
-thesaurus['naturewords'] = ['nature','personality','behavior','demeanor','tendencies','disposition','temperament']
-
-thesaurus['oddwords'] = ['remarkable','unusual','surprising','curious','peculiar','uncommon','odd']
-thesaurus['oftenwords'] = ['frequently','often','usually']
-
-thesaurus['peoplewords'] = ['many people','many races','some people','some races','some','many','numerous people','numerous peoples']
-thesaurus['predatorwords'] = ['predator','beast of prey','animal of prey','hunter','meat-eater','carnivore']
-
-thesaurus['rarelywords'] = ['infrequently','rarely','seldomly','occasionally','periodically']
-thesaurus['respectedwords'] = ['honored','respected','revered','exalted','venerated','regarded highly','highly regarded']
-
-thesaurus['seasonedwords'] = ['seasoned','accomplished','hardened','competent','expert','prepared']
-thesaurus['segadj'] = ['segmented','partitioned','compartmentalized']
-thesaurus['segwordplur'] = ['segments','sections','partitions']
-thesaurus['segwordsing'] = ['cavity','segment','section']
-thesaurus['shywords'] = ['shy','introverted','timid','reclusive','solitary']
-thesaurus['sizewords'] = ['size','stature']
-thesaurus['sometimeswords'] = ['sometimes','at times','occasionally','periodically']
-thesaurus['surprisinglywords'] = ['surprisingly','shockingly','amazingly','peculiarly','unusually','singularly']
-
-thesaurus['tendencywords'] = ['tendency','proclivity','propensity','predisposition']
-thesaurus['terrguardwords'] = ['guard','protect','defend','safeguard','maintain']
-thesaurus['terrifyingwords'] = ['terrifying','horrifying','frightening','daunting','unnerving','fearsome']
-thesaurus['territorywords'] = ['territory','territorial domain','territorial dominion','territorial stomping ground', \
-                  'territorial demesne']
-thesaurus['thoughwords'] = ['though','although','while','even though','whilst']
-thesaurus['travwordsplur'] = ['travelers','adventurers','wanderers','wayfarers']
-thesaurus['triflingwords'] = ['insignificant','unimpressive','trifling','trivial','slight']
-
-thesaurus['verywords'] = ['very','extremely','exceedingly','profoundly','remarkably','incredibly','excessively','extraordinarily','highly']
-thesaurus['visionwords'] = ['image','sight','vision','spectacle']
-
-thesaurus['weightwords'] = ['weight','heft','mass','bulk']
-
-def t(baseword):
-    global thesaurus
-    return thesaurus[baseword]
 
 ####################################
 # XVI. Main Worldgen Loop          #
